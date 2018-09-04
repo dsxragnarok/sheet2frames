d7c1aaa8eb9b3cb2e4cddba9571e5484453af41a
Kevin Phung <kevin.phung@nagrastar.com>
2018-09-04 17:59:24 -0600

    support outputDir option

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 const arguments = process.argv.slice(2);
 if (arguments < 2) {
-   console.info('Usage:\n\tsheet2frames inputfile columns [rows]');
+   console.info('Usage:\n\tsheet2frames inputfile [outputDir] columns [rows]');
    return process.exit(1);
 }
-
 const range = (n) => Array.from({ length: n }, (value, key) => key);
 
 const inputFile = arguments[0];
-const columns = parseInt(arguments[1]);
-const rows = arguments[2] ? parseInt(arguments[2], 10) : 1
+const outputDir = !Number.isInteger(parseInt(arguments[1]), 10) ? arguments[1] : '';
+const columns = arguments.length > 2 ? parseInt(arguments[2]) : parseInt(arguments[1], 10);
+const rows = arguments.length > 3 ? parseInt(arguments[3], 10) :
+            arguments.length === 3 && outputDir.length === 0 ? parseInt(arguments[2], 10) :
+            1;
 const basename = path.basename(inputFile).slice(0, -1 * path.extname(inputFile).length);
 
 Jimp.read(inputFile)
@@ -26,6 +28,7 @@ Jimp.read(inputFile)
       const x = column * frameWidth;
       const y = row * frameHeight;
       const filename = `${basename} (${frameIndex}).png`;
+      const outputPath = path.join(outputDir, filename);
 
       console.info('processing\tframe\tx\ty\twidth\theight');
       console.info(`\t\t${frameIndex}\t${x}\t${y}\t${frameWidth}\t${frameHeight}`);
@@ -38,8 +41,8 @@ Jimp.read(inputFile)
 
          const frame = sheet.clone().crop(x, y, frameWidth, frameHeight);
 
-         console.info(' -- saving', filename);
-         image.composite(frame, 0, 0).write(filename);
+         console.info(' -- saving', outputPath);
+         image.composite(frame, 0, 0).write(outputPath);
       });
    }));
 })
